56e0700fbdf498ff0b40bdc43834026279e1be35
atharvakunte <athu.kunte@gmail.com>
2020-11-13 19:18:14 +0530

    Initial commit
    Signed-off-by: atharvakunte <athu.kunte@gmail.com>

---
+++ index.js
@@ -0,0 +1,23 @@
+
+addTask = function(){
+    var task = document.getElementById('task').value
+    if(task===''){
+      alert("Please enter a task to add.")
+    }else{
+      document.getElementById('taskList').innerHTML=`<li class="list-group-item">
+    <div class="row">
+      <div class="col-10">
+        <p>${task}</p>
+      </div>
+      <div class="col-2">
+        <button class="btn btn-danger rounded" onclick="deleteTask(this)">Delete</button>
+      </div>
+    </div>
+  </li>`+document.getElementById('taskList').innerHTML
+  document.getElementById('task').value =''
+    }
+}
+
+deleteTask = function(btn){
+    btn.parentElement.parentElement.parentElement.remove()
+}
